e36d0e3a67123d6545fd3fde6d9563bd923102da
Shubham kahar <Shubhamkahar196@gmail.com>
2025-09-18 17:12:12 +0530

    fixing all error

--- app/components/LanguageSelector.tsx
+++ app/components/LanguageSelector.tsx
@@ -1,65 +1,65 @@
 "use client";
-
-import {useState} from "react";
+import React from "react";
+import { ChevronDown } from "lucide-react";
 import { languages } from "../utils/utilities";
 import OutsideClickHandler from "react-outside-click-handler";
-import { ChevronDown } from "lucide-react";
 
 interface LanguageSelectorProps {
-    language: string;
-    setLanguage: (language: string) => void;
-    setActiveIcon: (icon: string) => void;
+  language: string;
+  setLanguage: (language: string) => void;
+  seActiveIcon: (icon: string) => void;
 }
 
 function LanguageSelector({
-    language,
-    setLanguage,
-    setActiveIcon,
-}: LanguageSelectorProps){
+  language,
+  setLanguage,
+  seActiveIcon,
+}: LanguageSelectorProps) {
+  const [showDropdown, setShowDropdown] = React.useState(false);
 
-    const [showDropdown, setShowDropdown] = useState(false);
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
 
-    const toggleDropdown = () => {
-        setShowDropdown(!showDropdown);
+  const handleLanguageChange = (newLanguage: string) => {
+    setLanguage(newLanguage);
+    const newActiveIcon = languages.find(
+      (lang) => lang.name === newLanguage
+    )?.icon;
+
+    if (newActiveIcon) {
+      seActiveIcon(newActiveIcon);
     }
+  };
 
-    const handleLanguageChange = (newLanguage: string) =>{
-        setLanguage(newLanguage);
-        const newActiveIcon = languages.find(
-            (lang) => lang.name === newLanguage
-        )?.icon;
+  return (
+    <OutsideClickHandler onOutsideClick={() => setShowDropdown(false)}>
+      <div onClick={toggleDropdown}>
+        <p className="py-[5px] text-sm font-medium">Language</p>
+        <div className="dropdown-title capitalize w-[120px] hover:text-slate-50 transition-all duration-300 ease-in-out">
+          {language}
+          <ChevronDown />
+        </div>
 
-        if(newActiveIcon){
-            setActiveIcon(newActiveIcon);
-        }
-    };
-
-    return (
-        <OutsideClickHandler onOutsideClick ={() => setShowDropdown(false)}>
-          <div onClick={toggleDropdown}>
-            <p className="py-[5px] text-sm font-medium"> Language</p>
-            <div className="dropdown-title capitalize w-[120px] hover: text-slate-50 transition-all duration-300 ease-in-out">
-              {language}
-              <ChevronDown/>
-            </div>
-
-            {showDropdown && (
-                <div className="dropdown-menu w-[120px] top-[94px]">
-                 {languages.map((lang,i)=>{
-                    return (
-                        <div key={i}>
-                           <button className="dropdown-item text-left hover:text-slate-50 transition-all duration-300 ease-in-out" onClick={() => handleLanguageChange(lang.name)}>
-                            {lang.name}
-                           </button>
-                        </div>
-                    )
-                 })}
+        {showDropdown && (
+          <div className="dropdown-menu w-[120px] top-[94px]">
+            {languages.map((lang, i) => {
+              return (
+                <div key={i}>
+                  <button
+                    className="dropdown-item text-left hover:text-slate-50 transition-all duration-300 ease-in-out"
+                    onClick={() => handleLanguageChange(lang.name)}
+                  >
+                    {lang.name}
+                  </button>
                 </div>
-            )}
-
+              );
+            })}
           </div>
-        </OutsideClickHandler>
-    )
+        )}
+      </div>
+    </OutsideClickHandler>
+  );
 }
 
-export default LanguageSelector
+export default LanguageSelector;
